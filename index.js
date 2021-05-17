a5d85f91aed68ffcb9ad47e52220740eb048ccec
Brian Smith <brian.bubbam@gmail.com>
2021-05-17 18:35:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+function saturdayFun(saturdayFun="roller-skate") {
+   return `This Saturday, I want to ${saturdayFun}!`;
+}
+
+let mondayWork = function(workDay="go to the office") {
+    return `This Monday, I will ${workDay}.`;
+}  
+
+const wrapAdjective = function(symbol="*") {
+    return function(adjective="special") {
+        return `You are ${symbol}${adjective}${symbol}!`;
+    }
+}
